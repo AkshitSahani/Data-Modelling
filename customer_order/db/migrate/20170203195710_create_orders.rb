9353b6f6bb75e6cbe5470caebcc47ef8c11dfee3
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-03 15:05:48 -0500

    first data modelling and associations exercise

---
+++ customer_order/db/migrate/20170203195710_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.integer :order_number
+      t.integer :customer_id
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
